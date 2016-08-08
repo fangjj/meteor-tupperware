84ee52e68db426a5e54712f64f551dc775eb9b4d
fjj <fang_jimjim@163.com>
2016-08-08 14:26:51 +0800

    remove checkcode fun in update_node.sh and timeZone.sh

--- includes/scripts/_timezone.sh
+++ includes/scripts/_timezone.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 echo "setTimeZone $1"
 sudo echo $1 > /etc/timezone && sudo dpkg-reconfigure -f noninteractive tzdata
-check_code $?
--- includes/scripts/_update_node.sh
+++ includes/scripts/_update_node.sh
@@ -3,4 +3,3 @@ NODE_VERSION=v$1
 echo "update node $NODE_VERSION"
 npm install -g n
 n $NODE_VERSION
-check_code $?
