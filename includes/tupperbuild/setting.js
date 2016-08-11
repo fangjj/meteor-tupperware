e019904f87d0eba5ae3decec82a64ead17e09c14
fjj <fang_jimjim@163.com>
2016-08-11 10:05:56 +0800

    change handleExecError

--- includes/tupperbuild/setting.js
+++ includes/tupperbuild/setting.js
@@ -37,27 +37,19 @@ function StringAs(string) {
 }
 
 function handleExecError(done, cmd, taskDesc, error, stdout, stderr) {
-  if (! error) {
-      //if (stdout) {
-      //    console.log(stdout);
-      //}
-      //if (stderr) {
-      //    console.log(stderr);
-      //}
-    done();
-  } else {
-    log.error('While attempting to ' + taskDesc + ', the command:', cmd);
-    log.error('Failed with the exit code ' + error.code + '. The signal was ' + error.signal + '.');
     if (stdout) {
-      log.info('The task produced the following stdout:');
-      console.log(stdout);
+        console.log(stdout);
     }
     if (stderr) {
-      log.info('The task produced the following stderr:');
-      console.log(stderr);
+        console.log(stderr);
     }
-    suicide();
-  }
+      if (! error) {
+        done();
+      } else {
+        log.error('While attempting to ' + taskDesc + ', the command:', cmd);
+        log.error('Failed with the exit code ' + error.code + '. The signal was ' + error.signal + '.');
+        suicide();
+      }
 }
 
 /* Steps */
@@ -70,33 +62,11 @@ function setEnv (done) {
 	  var cmd = 'sh /tupperware/scripts/_setting.sh ' + settingsJsonStr;
 	  //log.info("cmd..."+cmd);
 	  log.info('Settings in settings.json registered.');
-	  var handler = child_process.exec(cmd, _.partial(handleExecError, done, cmd, 'set env'));
-        handler.stdout.on('data', function (data) {
-            log.info('stdout: ' + data);
-        });
-
-        handler.stderr.on('data', function (data) {
-            log.info('stderr: ' + data);
-        });
-
-        handler.on('exit', function (code) {
-            log.info('child process exited with code ' + code);
-        });
+        child_process.exec(cmd, _.partial(handleExecError, done, cmd, 'set env'));
 	} catch (e) {
 	  log.info('settings.json is not registered, please set METEOR_SETTINGS by yourself...');
 	  var cmd = 'sh /tupperware/scripts/_start_main.sh';
-        var handler = child_process.exec(cmd, _.partial(handleExecError, done, cmd, 'node main.js'));
-        handler.stdout.on('data', function (data) {
-            log.info(data);
-        });
-
-        handler.stderr.on('data', function (data) {
-            log.info(data);
-        });
-
-        handler.on('exit', function (code) {
-            log.info('child process exited with code ' + code);
-        });
+        child_process.exec(cmd, _.partial(handleExecError, done, cmd, 'node main.js'));
 	}
 }
 
