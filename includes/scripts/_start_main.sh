aae7df37a58dbd458b7c7e75ca02708151830a8d
fjj <fang_jimjim@163.com>
2016-08-12 13:32:17 +0800

    remove production env

--- includes/scripts/_start_main.sh
+++ includes/scripts/_start_main.sh
@@ -7,13 +7,9 @@ if [ ! -f $OUTPUT_DIR/bundle/main.js ]; then
   exit 1
 fi
 
-if [ -z "$NODE_ENV" ]; then
-  export NODE_ENV="production"
-fi
 
-if [ -z "$METEOR_ENV" ]; then
-  export METEOR_ENV="production"
-fi
+
+
 
 echo "[-] meteor-tupperware is starting your application with NODE_ENV=$NODE_ENV and METEOR_ENV=$METEOR_ENV..."
 node $OUTPUT_DIR/bundle/main.js
