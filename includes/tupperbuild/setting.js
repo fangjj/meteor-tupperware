504db03852e6fba5f79e05287bf8172504673a56
fjj <fang_jimjim@163.com>
2016-08-15 15:14:31 +0800

    fix conflict

--- includes/tupperbuild/setting.js
+++ includes/tupperbuild/setting.js
@@ -12,6 +12,10 @@ var copyPath = '/app',
 
 var log = {};
 
+var exec_options = {
+    maxBuffer: 2*1024*1024  /*stdout和stderr的最大长度*/
+};
+
 log.info = function () {
   var args = Array.prototype.slice.apply(arguments);
   args.splice(0, 0, '[-] ');
@@ -58,20 +62,20 @@ function handleExecError(done, cmd, taskDesc, error, stdout, stderr) {
 
 function setEnv (done) {
     log.info('setting Env commands...');
-    var cmd = '/tupperware/scripts/_setting.sh';
+    var cmd = '';
     var settingsJsonStr = '';
 	try {
-	  settingsJson = require(copyPath + '/settings.json');
-      settingsJsonStr = StringAs(JSON.stringify(settingsJson));
-	} catch (e) {
+	    settingsJson = require(copyPath + '/settings.json');
+        settingsJsonStr = StringAs(JSON.stringify(settingsJson));
+        cmd = 'sh /tupperware/scripts/_setting.sh ' + settingsJsonStr;
+    } catch (e) {
 	  log.info('settings.json is not registered, please set METEOR_SETTINGS by yourself...');
+        cmd = 'sh /tupperware/scripts/_start_main.sh';
 	} finally {
-        var paramArray = [cmd];
         if(settingsJsonStr){
             log.info('Settings in settings.json registered.');
-            paramArray.push(settingsJsonStr);
         }
-        var child = child_process.spawn('sh',paramArray,{stdio:'pipe'});
+        var child = child_process.exec(cmd,exec_options);
         if(child){
             child.stdout.on('data', function(data) {
                 console.log(data.toString('utf-8'));
