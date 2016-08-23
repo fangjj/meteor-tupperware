37a1e0566cd9207c57f5c3e4467b9ba605d97f67
fjj <fang_jimjim@163.com>
2016-08-23 14:08:51 +0800

    add npmTaobao from tupperware.json

--- includes/scripts/_npm_taobao.sh
+++ includes/scripts/_npm_taobao.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
 echo "npm taobao..."
 npm config set registry https://registry.npm.taobao.org
-check_code $?
-
 npm config set disturl https://npm.taobao.org/dist
-check_code $?
-
 npm config get registry
-check_code $?
