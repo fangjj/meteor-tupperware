b81f673e1faff6801ff122a14bc302076bbec9dd
fangjimjim <fangjimjim@qq.com>
2018-08-27 14:30:32 +0800

    update

--- includes/scripts/_install_node.sh
+++ includes/scripts/_install_node.sh
@@ -6,6 +6,9 @@ echo "[-] Installing Node.js... $NODE_DIST"
 
 cd /tmp
 
+curl -SLO "http://nodejs.org/dist/v$NODE_VERSION/$NODE_DIST.tar.gz"
+check_code $?
+
 tar -xzf "$NODE_DIST.tar.gz" -C /usr/local --strip-components=1
 check_code $?
 
